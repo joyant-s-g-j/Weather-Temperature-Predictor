c18949c8b3e300957ef68dba5d23c66e39ae8909
joyantgupta <joyantsheikharguptajoy@gmail.com>
2026-01-17 06:55:04 +0600

    packages

---
+++ rf_train.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import  r2_score, mean_squared_error, mean_absolute_error
+
