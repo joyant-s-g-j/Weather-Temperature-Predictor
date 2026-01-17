1f919d4a23c8d78c419caf9ba20fcc0e4d6d3c51
joyantgupta <joyantsheikharguptajoy@gmail.com>
2026-01-17 06:56:08 +0600

    data load

--- rf_train.py
+++ rf_train.py
@@ -10,3 +10,11 @@ from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import  r2_score, mean_squared_error, mean_absolute_error
 
+city_attr = pd.read_csv("dataset/city_attributes.csv")
+humidity = pd.read_csv("dataset/humidity.csv")
+pressure = pd.read_csv("dataset/pressure.csv")
+temperature = pd.read_csv("dataset/temperature.csv")
+weather_desc = pd.read_csv("dataset/weather_description.csv")
+wind_dir = pd.read_csv("dataset/wind_direction.csv")
+wind_speed = pd.read_csv("dataset/wind_speed.csv")
+
